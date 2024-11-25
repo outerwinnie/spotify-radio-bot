46a59cba6af7a2ddd228417f45085c922d76a95d
Winnie <outerwinnie@protonmail.com>
2024-11-25 22:03:44 +0100

    Missing files

--- obj/Debug/net9.0/spotify-radio-bot.AssemblyInfo.cs
+++ obj/Debug/net9.0/spotify-radio-bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spotify-radio-bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb2f218827d2c45bb98c1f3534664286132e06b")]
 [assembly: System.Reflection.AssemblyProductAttribute("spotify-radio-bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spotify-radio-bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
